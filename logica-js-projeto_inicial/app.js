7487d24e1b58799eea46e6d7163bb65fe1f4d2a6
Douglas Soffi <douglas.soffi@protonmail.com>
2025-09-30 10:35:05 -0300

    Segundo capitulo do curso e realizando novos desafios

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,5 @@
 // Chalanges
+// 1
 alert('Welcome to our site!');
 let name = 'Moon';
 let age = 25;
@@ -15,11 +16,39 @@ if (userAge >= 18) {
 else {
     alert('Can not drive!');
 }
+
+// 2
+let day = prompt('What week day is today?');
+if (day == 'Sunday' || day == 'Saturday') {
+    alert('Happy weekend!');
+} else {
+    alert('Happy week!');
+}
+let checkNumber = prompt('Choose a number: ');
+if (checkNumber < 0) {
+    alert('Negative');
+} else if (checkNumber > 0) {
+    alert('Positive');
+} else {
+    alert('0');
+}
+let score = 110;
+if (score >= 100) {
+    alert('You win');
+} else {
+    alert('You loose');
+}
+let money = 1000;
+alert(`You have $${money}.`);
+alert(`Your name is ${userName}.`);
 // Chalanges
 
-let secretNumber = 50;
+let secretNumber = 30;
+console.log(secretNumber);
 let chosenNumber = prompt('Choose a number between 1 and 30.'); 
 
 if (secretNumber == chosenNumber) {
-    console.log('You are right.');
+    alert(`You are right. ${secretNumber}`);
+} else {
+    alert(`You are wrong. ${secretNumber}`);
 }
