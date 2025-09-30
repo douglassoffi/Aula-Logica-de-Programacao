5bcdf7d0a8a67c568e080c8793bf70ac1dc331e0
Douglas Soffi <douglas.soffi@protonmail.com>
2025-09-30 10:00:49 -0300

    Criando repositorio e realizando desafios propostos

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,25 @@
+// Chalanges
+alert('Welcome to our site!');
+let name = 'Moon';
+let age = 25;
+let salesNumber = 50;
+let availableBalance = 1000;
+alert('Error.');
+let errorMassage = 'Error';
+alert(errorMassage);
+let userName = prompt('Tell us your name: ');
+let userAge = prompt('Tell us your age: ');
+if (userAge >= 18) {
+    alert('Can drive!');
+}
+else {
+    alert('Can not drive!');
+}
+// Chalanges
+
+let secretNumber = 50;
+let chosenNumber = prompt('Choose a number between 1 and 30.'); 
+
+if (secretNumber == chosenNumber) {
+    console.log('You are right.');
+}
